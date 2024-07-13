a032d1c32ed62841e9aa3d4a9a808288e6ccfc80
Will Hawkins <hawkinsw@obs.cr>
2024-07-12 20:22:02 -0400

    Initial work on activation records issue.
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

---
+++ main.c
@@ -0,0 +1,16 @@
+int add(int a, int b) {
+    int result = a + b;
+    return result;
+}
+
+int multiply_by_two(int a) {
+    int multiplied = a * 2;
+    return multiplied;
+}
+
+int main() {
+    int five = 5;
+    int five_times_two = multiply_by_two(five);
+    int sum = add(five, five_times_two);
+    return 0;
+}
