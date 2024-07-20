ad272631dd5d14e7d2c1a3ba8c6be3ab345c9b88
Will Hawkins <hawkinsw@obs.cr>
2024-07-20 01:57:40 -0400

    Remove What's News for Activation Record issue
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- main.c
+++ main.c
@@ -4,13 +4,13 @@ int add(int a, int b) {
 }
 
 int multiply_by_two(int a) {
-    int multiplied = a * 2;
+    int multiplied = add(a,a);
     return multiplied;
 }
 
 int main() {
     int five = 5;
-    int five_times_two = multiply_by_two(five);
+    int five_times_two = /* HANDOFF */ multiply_by_two(five);
     int sum = add(five, five_times_two);
     return 0;
 }
